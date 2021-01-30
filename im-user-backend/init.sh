f7888f6f187db00f616ca0911e43f6d63f936be3
diegopm2000 <diegopm2000@gmail.com>
2021-01-30 18:34:59 +0100

    port changed

--- im-user-backend/init.sh
+++ im-user-backend/init.sh
@@ -3,7 +3,7 @@
 # Execute this script as source ./init_git.sh for export variables to global environment outside the script scope
 
 # Express Port
-export EXPRESS_PORT="8080"
+export EXPRESS_PORT="8081"
 env | grep '^EXPRESS_PORT='
 
 # Mongo URL
